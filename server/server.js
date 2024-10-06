9a520b19442b4ed3c86c2efd1096c7d346721c7d
srinivasan-ad <forprogramingpurpose@gmail.com>
2024-10-06 11:56:04 +0530

    Trying :zap:

--- server/server.js
+++ server/server.js
@@ -1,14 +1,15 @@
 const express = require("express");
 const app = express();
-
+const cors = require('cors')
+app.use(cors())
 
 app.use(express.json());
 app.listen(5000, () => {
     console.log("Server has started on port 5000");
   });
-  app.post("/exoplanet-calculator", async (req, res) => {
+  app.post("/habitable-exoplanet-calculator", async (req, res) => {
     try{
-const {R_star,L_star,T_eff,R_planet,M_planet,semi_major_axis, eccentricity,inclination,albedo,distance } = req.body
+const {R_star,L_star,T_eff,R_planet,M_planet,semi_major_axis, eccentricity,inclination,albedo,distance,D_telescope,wavelength,IWA,OWA,contrast_limit } = req.body
 console.log("Received data:", req.body);
 res.status(200).json({ message: "Data received successfully", data: req.body });
 }
